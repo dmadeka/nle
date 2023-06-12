6a7f975da182b8ea2503d520cfa7fa926aa7e7c7
Dhruv Madeka <madeka@bu.edu>
2023-06-12 02:21:35 -0400

    Fixes to syntax

--- src/allmain.c
+++ src/allmain.c
@@ -50,14 +50,14 @@ boolean resuming;
         (void) enter_explore_mode();
 
     /* side-effects from the real world */
-    flags.moonphase = 0 /* phase_of_the_moon(); */
+    flags.moonphase = 0; /* phase_of_the_moon(); */
     if (flags.moonphase == FULL_MOON) {
         You("are lucky!  Full moon tonight.");
         change_luck(1);
     } else if (flags.moonphase == NEW_MOON) {
         pline("Be careful!  New moon tonight.");
     }
-    flags.friday13 = 0 /* friday_13th(); */
+    flags.friday13 = 0; /* friday_13th(); */
     if (flags.friday13) {
         pline("Watch out!  Bad things can happen on Friday the 13th.");
         change_luck(-1);
